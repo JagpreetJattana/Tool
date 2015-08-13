56184ae4c1e0050a7b914d06491edc616ec1e260
jagpreetjattana <jagpreet@hotmail.com>
2015-08-13 17:47:38 -0400

    asd

---
+++ Jagpreet-Jattana_test/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace Jagpreet_Jattana_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shelter sht = new Shelter(10);
+            Console.Write("Currently there are " + sht.bookedShelter + " shelter bookings are to be cancelled");
+            Booth bthA = new Booth("A", sht);
+            Booth bthB = new Booth("B", sht);
+
+            Thread t1 = new Thread(new ThreadStart(bthA.performCancelation));
+            Thread t2 = new Thread(new ThreadStart(bthB.performCancelation));
+
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("\n\rCancellations made by Booth A = "+bthA.getNoOfCancellations());
+            Console.WriteLine("Cancellations made by Booth B = " + bthB.getNoOfCancellations());
+
+            XDocument myDocument = XmlDataClass.createData();
+            Console.WriteLine(myDocument.ToString());
+            Console.ReadKey();
+
+
+             
+            XmlDataClass.SaveDataToFile("booking_cancelation.xml");
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+
+    class Shelter {
+
+       public int bookedShelter;
+
+          
+
+            public Shelter(int bs)
+            {
+                this.bookedShelter = bs;
+            }
+            public void cancelBooking()
+            {
+                lock (this)
+                {
+                    this.bookedShelter--;
+                    Monitor.Pulse(this);
+                }
+            }
+          
+    
+    
+    
+    }
+
+    class Booth {
+        Shelter shlt;
+        String bothName;
+        int noCancelbyBoth;
+        String list;
+
+        public Booth(String bothname,Shelter shelter)
+        {
+            this.shlt = shelter;
+            this.bothName = bothname;
+        }
+
+        public int getNoOfCancellations()
+        {
+            return this.noCancelbyBoth;
+        }
+
+        public void performCancelation() {
+
+            while (this.shlt.bookedShelter > 0)
+            {
+                this.shlt.cancelBooking();
+                this.noCancelbyBoth++;
+                this.list += shlt.bookedShelter+" ";
+                Console.WriteLine("\n\rBooth "+this.bothName +  " cancelled a booking. The current number of bookings to cancel - " + shlt.bookedShelter);
+                Console.WriteLine("\n\rBooth "+this.bothName+" have cancelled "+this.noCancelbyBoth+" shelter bookings so far");
+                Console.WriteLine("These are "+this.list);
+                Console.WriteLine("\n\rEnd of cancellation by booth " + this.bothName);
+                Thread.Sleep(1000);
+            }
+        }
+    
+    }
+
+}
---
+++ Jagpreet-Jattana_test/XmlDataClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Jagpreet_Jattana_test
+{
+    class XmlDataClass
+    {
+       public String A, B;
+        public XmlDataClass(int a,int b) {
+
+            this.A = a.ToString();
+            this.B = b.ToString();
+        }
+        public static XDocument createData()
+        {
+
+         /*   XDocument playerData = new XDocument(
+                new XDeclaration("1.0", "utf8", "yes"),
+                new XComment("Creating XML file for the records of booking cancellations"),
+                new XElement("ABCPark",
+
+                new XElement("Both A",
+                    new XAttribute("Name", "Booth A"),
+                            new XAttribute("Shelters","asd" ),
+                new XElement("Both B",
+                    new XAttribute("Name", "Both B"),
+                            new XAttribute("Shelters", "asd"))
+
+
+                )
+            );*/
+
+            XDocument playerData = new XDocument(
+             new XDeclaration("1.0", "utf8", "yes"),
+             new XComment("Creating XML file"),
+             new XElement("ABCPark",
+
+             new XElement("Booth",
+                 new XElement("Name", "Booth A"),
+                 
+                 new XElement("Shelter","")),
+             new XElement("Booth",
+                 new XElement("Name", "Booth B"),
+               
+                 new XElement("Shelter",""))
+
+
+             )
+         );
+
+            return playerData;
+        
+        
+        }
+
+        public static void SaveDataToFile(string filename)
+        {
+            XDocument document = createData();
+            document.Save(filename);
+        }
+
+
+
+
+
+    }
+}
